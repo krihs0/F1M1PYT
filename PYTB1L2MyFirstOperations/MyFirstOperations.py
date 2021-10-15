811fcd8313f5443419889268c39bc50718f4f0e6
Krins <32813@ma-web.nl>
2021-10-15 12:40:16 +0200

    firstoperations

---
+++ PYTB1L2MyFirstOperations/MyFirstOperations.py
@@ -0,0 +1,18 @@
+X = 0
+print("Het begingetal is " + X )
+X += 5
+print("Daar 5 bij optellen is " + X)
+X -= 3
+print("Daar 3 van af halen is " + X)
+X = X * 5
+print("Dat keer 5 is " + X)
+X = X / 3
+print("Dat delen door 3 is " + X)
+
+##er zijn meer operators maar dat kan ik niet echt in dit verhaal zetten dus die zet ik hieronder
+
+  #  %
+  #  +=
+  #  -=
+  #  /=
+  #  ==
