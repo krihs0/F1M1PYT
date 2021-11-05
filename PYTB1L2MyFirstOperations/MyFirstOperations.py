19e46f5d1f9077e5c9974b14f5e8feb503887a49
Krins <32813@ma-web.nl>
2021-11-05 09:49:42 +0100

    slicewise

--- PYTB1L2MyFirstOperations/MyFirstOperations.py
+++ PYTB1L2MyFirstOperations/MyFirstOperations.py
@@ -1,13 +1,13 @@
 X = 0
-print("Het begingetal is " + X )
+print("Het begingetal is " + str(X))
 X += 5
-print("Daar 5 bij optellen is " + X)
+print("Daar 5 bij optellen is " + str(X))
 X -= 3
-print("Daar 3 van af halen is " + X)
+print("Daar 3 van af halen is " + str(X))
 X = X * 5
-print("Dat keer 5 is " + X)
+print("Dat keer 5 is " + str(X))
 X = X / 3
-print("Dat delen door 3 is " + X)
+print("Dat delen door 3 is " + str(X))
 
 ##er zijn meer operators maar dat kan ik niet echt in dit verhaal zetten dus die zet ik hieronder
 
---
+++ PYTB1L2SliceWise/slicewise.py
@@ -0,0 +1,18 @@
+tekst = 'De Python lessen worden gegeven voor Erik, Erwin en ook door Hidde'
+print(tekst[37:41])
+print(tekst[43:48])
+print(tekst[61:66])
+
+tekst = 'SD vakken zijn bijvoorbeeld Python, UXD, Frontend development en Backend development. Wat leuk!'
+print(tekst[28:34])
+print(tekst[36:39])
+print(tekst[41:61])
+print(tekst[65:84])
+
+tekst = 'Wat is hier het laatste woord?'
+#idk of ? ook als letter geld maar neem an van niet
+print(tekst[22:29])
+
+tekst = 'Het www is ontwikkeld vanaf 1989 door Tim Berners-Lee'
+print(tekst[5:10])
+print(tekst[34:40])
